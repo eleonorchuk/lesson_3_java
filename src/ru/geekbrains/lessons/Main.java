203f66a919ce2b5683933f44c5d8117c5f1163dc
eleonorchuk <zoimy@ukr.net>
2021-09-12 23:25:19 +0300

    task 2

--- src/ru/geekbrains/lessons/Main.java
+++ src/ru/geekbrains/lessons/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
         // Test task 1
         invertArray();
+
+        // Test task 2
+        initArray();
     }
 
     /*
@@ -25,6 +28,19 @@ public class Main {
                 array[i] = 1;
         }
         System.out.println("Output array = " + Arrays.toString(array));
+    }
 
+    /*
+    2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+    */
+    public static void initArray() {
+        System.out.println("initArray");
+        int[] array = new int[100];
+        System.out.println("Input array  = " + Arrays.toString(array));
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        System.out.println("Output array = " + Arrays.toString(array));
     }
 }
